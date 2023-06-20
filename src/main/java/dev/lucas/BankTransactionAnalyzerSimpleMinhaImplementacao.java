d938353e332663818603362d47cc8731bfee276d
Lucas Silva <lcsd.lucas@gmail.com>
2023-06-19 23:32:07 -0300

    Melhorando o código baseado no código do livro

--- src/main/java/dev/lucas/BankTransactionAnalyzerSimpleMinhaImplementacao.java
+++ src/main/java/dev/lucas/BankTransactionAnalyzerSimpleMinhaImplementacao.java
@@ -2,6 +2,10 @@ package dev.lucas;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,40 +19,49 @@ import static java.util.Comparator.comparingInt;
  */
 public class BankTransactionAnalyzerSimpleMinhaImplementacao {
 
-    public static void main(String[] args) throws FileNotFoundException {
-//        System.out.println("Hello World!");
+    private final static String root = "src/main/resources/";
 
-        var scanner = new Scanner(new FileReader("src/main/java/extrato-modelo.csv"));
+    public static void main(String[] args) throws IOException {
+        var path = Paths.get(root + "extrato-modelo.csv");
 
-        List<Gastos> extrato = new ArrayList<>();
-
-        while (scanner.hasNextLine()) {
-
-            String[] split = scanner.nextLine().split(",");
-
-            extrato.add(new Gastos(split[0], split[1], split[2]));
-
+        double total = 0d;
+        for (String registro : Files.readAllLines(path)) {
+            total += Double.parseDouble(registro.split(",")[1]);
         }
 
-        var listaDeEntradas = extrato.stream().filter(e -> Integer.parseInt(e.valor) > 0).toList();
-        System.out.println("Lista de Entradas -> " + listaDeEntradas);
+        System.out.println("The total for all transactions is " + total);
 
-        var listaDeSaidas = extrato.stream().filter(e -> Integer.parseInt(e.valor) < 0).toList();
-        System.out.println("Lista de Saidas -> " + listaDeSaidas);
-
-
-        var dataFiltro = "01";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        var listaMesFiltro = extrato.stream().filter(e -> LocalDate.parse(e.data,formatter).getMonth().getValue() == Integer.parseInt(dataFiltro)).toList();
-        System.out.println("Filtrando por mês -> " + listaMesFiltro);
-
-
-//        Comparator<Gastos> gastosComparator = (o1, o2) -> 0;
-        var top10Gastos = extrato.stream().filter(e -> Integer.parseInt(e.valor) < 0)
-                .sorted(comparingInt(x -> Integer.parseInt(x.valor)))
-                .limit(10).toList();
-
-        System.out.println("Top 10 Gastos -> " + top10Gastos);
+//        var scanner = new Scanner(new FileReader("src/main/java/extrato-modelo.csv"));
+//
+//        List<Gastos> extrato = new ArrayList<>();
+//
+//        while (scanner.hasNextLine()) {
+//
+//            String[] split = scanner.nextLine().split(",");
+//
+//            extrato.add(new Gastos(split[0], split[1], split[2]));
+//
+//        }
+//
+//        var listaDeEntradas = extrato.stream().filter(e -> Integer.parseInt(e.valor) > 0).toList();
+//        System.out.println("Lista de Entradas -> " + listaDeEntradas);
+//
+//        var listaDeSaidas = extrato.stream().filter(e -> Integer.parseInt(e.valor) < 0).toList();
+//        System.out.println("Lista de Saidas -> " + listaDeSaidas);
+//
+//
+//        var dataFiltro = "01";
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//        var listaMesFiltro = extrato.stream().filter(e -> LocalDate.parse(e.data,formatter).getMonth().getValue() == Integer.parseInt(dataFiltro)).toList();
+//        System.out.println("Filtrando por mês -> " + listaMesFiltro);
+//
+//
+////        Comparator<Gastos> gastosComparator = (o1, o2) -> 0;
+//        var top10Gastos = extrato.stream().filter(e -> Integer.parseInt(e.valor) < 0)
+//                .sorted(comparingInt(x -> Integer.parseInt(x.valor)))
+//                .limit(10).toList();
+//
+//        System.out.println("Top 10 Gastos -> " + top10Gastos);
 
 
     }
